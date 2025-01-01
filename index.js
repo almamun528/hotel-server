f70003c0bf2fd9920d850a8788a469dfd3f7ceb4
almamun528 <ayapths@gmail.com>
2025-01-01 13:01:12 +0600

    setup backend

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const cors = require('cors')
+require("dotenv").config();
+const port = process.env.PORT || 3000 
+const app = express()
+// middleWear
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    console.log('Server is running into get-operation index true')
+})
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =`mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.34ihq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const hotelCollection = client.db('hotelCollection').collection('hotel')
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, ()=>{
+    console.log(`app is running on port ${port}`)
+})
+
+// USER_NAME=hotel-server
+// PASSWORD=qBCnDgBcMW3aVGK3
